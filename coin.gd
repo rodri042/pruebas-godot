cbd2efe2e3a09b81101d0b88f2ed9368c6dedfb1
Rodrigo Alfonso <rodri042@gmail.com>
2018-06-19 23:34:36 -0300

    Agregamos moneditas

---
+++ coin.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_coin_body_entered(body):
+	$sound.play()
+	visible = false
+	pass # replace with function body
+
+
+func _on_sound_finished():
+	queue_free()
+	pass # replace with function body
